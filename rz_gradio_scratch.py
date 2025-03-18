2f21b939ed2a20a93e1e7f687496909272ede930
Rob Zimmelman <rob.zimmelman@buck.co>
2025-03-18 13:51:11 -0400

    add optional filename parameter to create function for custom image naming

--- rz_gradio_scratch.py
+++ rz_gradio_scratch.py
@@ -8,7 +8,7 @@ from pprint import pprint as pp
 
 
 
-def create(user_prompt:str=None,  focal_length_in_mm:int=None, seed:int=None):
+def create(user_prompt:str=None,  focal_length_in_mm:int=None, seed:int=None, filename:str=None):
     print(user_prompt)
     prompt = {
     "sources": [
@@ -53,6 +53,8 @@ def create(user_prompt:str=None,  focal_length_in_mm:int=None, seed:int=None):
         dashed_prompt_with_seed = dashed_prompt + f"_seed_{seed}"
         dashed_prompt_with_seed = dashed_prompt_with_seed[:128]
         image_name_from_prompt = f"{dashed_prompt_with_seed}.png"
+        if filename and filename is not None:
+            image_name_from_prompt = f"{filename}.png"
         # image_name = 'zoop.png'
         img.save(image_name_from_prompt, "PNG")
         return image_name_from_prompt
@@ -62,9 +64,9 @@ def create(user_prompt:str=None,  focal_length_in_mm:int=None, seed:int=None):
 
 
 
-def get_stuff(theprompt:str=None, focal_length:int=None, seed:int=None):
+def get_stuff(theprompt:str=None, focal_length:int=None, seed:int=None, filename:str=None):
     gr.Info("Running Prompt Create")
-    myimg_name = create(user_prompt=theprompt, focal_length_in_mm=focal_length, seed=seed)
+    myimg_name = create(user_prompt=theprompt, focal_length_in_mm=focal_length, seed=seed, filename=filename)
     my_image =gr.Image(value=myimg_name)
     # copy_result = shutil.copy('zoop.png', myimg_name)
     # print(copy_result)
@@ -99,7 +101,8 @@ demo = gr.Interface(
             # gr.Checkbox(value=True, label="Secret3 Checkbox"),
             gr.Text(label="Enter a Prompt"),
             gr.Text(label="Focal Length in mm"),
-            gr.Text(label="Seed Integer for Render Randomness")
+            gr.Text(label="Seed Integer for Render Randomness"),
+            gr.Text(label="Enter a File Name or Leave Blank to Name with First 128 Chars of Prompt")
             # gr.Slider(value=15, minimum=0, maximum=100, step=1, label="Camera Focal Length"),
             # gr.JSON(value=cam_defs, label= "Camera Definition"),
             # gr.FileExplorer(glob="*.txt", label="Select A File To Upload")
